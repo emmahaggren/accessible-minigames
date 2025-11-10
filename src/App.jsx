4edaded24564f32308c8d27c57418a55ac837c13
lola.sh <aurorashuti@gmail.com>
2025-11-10 13:57:48 +0100

    frontpage

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,41 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="page">
+      <div className="container"> 
+        <h1 className="title-text">Welcome!</h1>
+        <div className='icons'>
+          <a href="memory.html">
+           <img
+            src="https://eclass.asfa.gr/courses/h5p/libraries/H5P.MemoryGame-1.3/icon.svg"
+            alt="Memory game"
+            className="icon memory-icon"
+           />
+          </a>
+
+
+          <a href="quiz.html">
+            <img
+            src="https://www.drppt.org/wp-content/uploads/QUIZ-3D.png"
+            alt="Quiz game"
+            style={{ width: '170px', height: '170px' }}
+            className="icon quiz-icon"
+            />
+          </a>
+          <a href="jump.html">
+            <img
+              src="https://geekanant.gallerycdn.vsassets.io/extensions/geekanant/chrome-dinosaur-game/0.0.1/1638086697139/Microsoft.VisualStudio.Services.Icons.Default"
+              alt="Jump game"
+              style={{ width: '170px', height: '170px' }}
+              className="icon jump-icon"
+            />
+          </a>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <img src="src/images/cat.png" alt="Cat waving" className="cat" />
+    </div>
+   
   )
 }
 
